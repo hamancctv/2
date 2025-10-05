404b411c9cd8b6d5a6edd78e17e48b333cc233fb
hamancctv <110594838+hamancctv@users.noreply.github.com>
2025-10-06 03:40:12 +0900

    Update markers-handler.js

--- markers-handler.js
+++ markers-handler.js
@@ -114,9 +114,7 @@
       "https://t1.daumcdn.net/localimg/localimages/07/2018/pc/img/marker_spot.png",
       new kakao.maps.Size(36,50.4),
       {
-        offset:new kakao.maps.Point(18,50.4),
-        shape:"rect",
-        coords:"0,0,36,56"
+        offset:new kakao.maps.Point(18,50.4)
       }
     );
 
