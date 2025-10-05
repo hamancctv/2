0daab2a9887b01173bb3b67112e56f8d33d31242
hamancctv <110594838+hamancctv@users.noreply.github.com>
2025-10-06 05:41:07 +0900

    Update markers-handler.js

--- markers-handler.js
+++ markers-handler.js
@@ -111,7 +111,7 @@
     );
 
     hoverImage = new kakao.maps.MarkerImage(
-      "https://hamancctv.github.io/2/hover_marker.png",
+      "https://hamancctv.github.io/2/marker_spot (2).png",
       new kakao.maps.Size(36,60.4), { offset:new kakao.maps.Point(18,50.4)
       }
     );
